2058d87a5fc8f153694f0eb0fdb0b9e14ce3aa78
lourensoberholzer45-ship-it <lourensoberholzer45@gmail.com>
2026-06-22 09:22:37 +0200

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,31 @@
+/* RoadLog Service Worker */
+const CACHE = 'roadlog-v1';
+const ASSETS = [
+  '/roadlog-app/',
+  '/roadlog-app/index.html',
+  '/roadlog-app/manifest.json',
+  '/roadlog-app/icon-192.png',
+  '/roadlog-app/icon-512.png'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE).then(c => c.addAll(ASSETS))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(cached => cached || fetch(e.request))
+  );
+});
